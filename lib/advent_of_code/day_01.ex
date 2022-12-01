395861aec0c2bd83f27f7e0ee555193cf0448a36
Julian Köpke <julian.koepke@gmail.com>
2022-12-01 23:28:34 +0100

    day 01

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,32 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    parse(args) |> Enum.max()
+  end
+
+  defp parse(args) do
+    for d <- String.split(String.trim(args), "\n\n") do
+      for b <- String.split(d, "\n") do
+        Integer.parse(b) |> elem(0)
+      end
+      |> Enum.sum()
+    end
   end
 
   def part2(args) do
+    for i <- parse(args), reduce: [] do
+      acc ->
+        cond do
+          Enum.count(acc) < 3 ->
+            [i | acc]
+
+          Enum.all?(acc, fn e -> e > i end) ->
+            acc
+
+          true ->
+            a = Enum.sort(acc) |> Enum.drop(1)
+            [i | a]
+        end
+    end
+    |> Enum.sum()
   end
 end

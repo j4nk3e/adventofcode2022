7fe224a7d453d86e5dfc348e3f9728cebc1850cb
Julian Köpke <julian.koepke@gmail.com>
2022-12-02 09:54:46 +0100

    day 02

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(args) do
+    parse(args)
+    |> Enum.map(fn [other, me] ->
+      me + 1 +
+        case rem(me - other + 3, 3) do
+          0 -> 3
+          1 -> 6
+          2 -> 0
+        end
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp parse(args) do
+    plays = args |> String.trim() |> String.split("\n")
+
+    for p <- plays do
+      String.split(p, " ")
+      |> Enum.map(fn c ->
+        cond do
+          c == "A" || c == "X" -> 0
+          c == "B" || c == "Y" -> 1
+          c == "C" || c == "Z" -> 2
+        end
+      end)
+    end
+  end
+
+  def part2(args) do
+    parse(args)
+    |> Enum.map(fn [other, me] ->
+      rem(other + me - 1 + 3, 3) + 1 + me * 3
+    end)
+    |> Enum.sum()
   end
 end

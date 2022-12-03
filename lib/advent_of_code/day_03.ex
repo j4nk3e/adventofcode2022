92b3532b8c729822d91ac91c3516ced3920d7835
Julian Köpke <julian.koepke@gmail.com>
2022-12-03 10:54:15 +0100

    day 03

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  import MapSet
+
+  def part1(args) do
+    for l <- parse(args), len = String.length(l) do
+      [a, b] =
+        to_charlist(l)
+        |> Enum.split(div(len, 2))
+        |> Tuple.to_list()
+        |> Enum.map(&MapSet.new/1)
+
+      intersection(a, b) |> to_list |> hd |> cost
+    end
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp cost(c) when c < ?a, do: c - ?A + 27
+  defp cost(c), do: c - ?a + 1
+
+  defp parse(args), do: String.trim(args) |> String.split("\n")
+
+  def part2(args) do
+    groups =
+      parse(args)
+      |> Enum.map(&to_charlist/1)
+      |> Enum.map(&MapSet.new/1)
+      |> Enum.chunk_every(3)
+
+    for [a, b, c] <- groups do
+      intersection(a, b)
+      |> intersection(c)
+      |> to_list
+      |> hd
+      |> cost
+    end
+    |> Enum.sum()
   end
 end

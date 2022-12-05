3c41e3ab87361db8bb4fec091e7761640f1eec54
Julian Köpke <julian.koepke@gmail.com>
2022-12-05 09:03:40 +0100

    day 05

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,82 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(args) do
+    {map, inst} = parse(args)
+
+    for [n, from, to] <- inst, reduce: map do
+      acc ->
+        f = acc[from]
+        m = Enum.take(f, n) |> Enum.reverse()
+
+        acc
+        |> Map.replace!(from, Enum.drop(f, n))
+        |> Map.update!(to, fn l -> m ++ l end)
+    end
+    |> top_string()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    {map, inst} = parse(args)
+
+    for [n, from, to] <- inst, reduce: map do
+      acc ->
+        f = acc[from]
+        m = Enum.take(f, n)
+
+        acc
+        |> Map.replace!(from, Enum.drop(f, n))
+        |> Map.update!(to, fn l -> m ++ l end)
+    end
+    |> top_string
+  end
+
+  defp top_string(map) do
+    map
+    |> Map.to_list()
+    |> Enum.sort_by(fn {i, _} -> i end)
+    |> Enum.map(fn {_, e} -> hd(e) end)
+    |> Enum.join()
+  end
+
+  defp parse(args) do
+    [stack, inst] = args |> String.split("\n\n")
+
+    s =
+      for l <- stack |> String.split("\n"), !String.starts_with?(l, " 1") do
+        Regex.scan(~r/[(\w)]|\s{4}/, l)
+        |> Enum.map(&hd/1)
+      end
+      |> Enum.reverse()
+
+    len = s |> Enum.map(&Enum.count/1) |> Enum.max()
+
+    map =
+      List.duplicate([], len)
+      |> Enum.with_index()
+      |> Enum.map(fn {a, b} -> {b + 1, a} end)
+      |> Map.new()
+
+    map =
+      for r <- s, reduce: map do
+        map ->
+          Enum.reduce(
+            Enum.with_index(r),
+            map,
+            fn {c, i}, acc ->
+              if String.starts_with?(c, " ") do
+                acc
+              else
+                Map.update!(acc, i + 1, fn l -> [c | l] end)
+              end
+            end
+          )
+      end
+
+    inst =
+      for i <- inst |> String.trim() |> String.split("\n") do
+        [_, a, b, c] = Regex.run(~r/move (\d+) from (\d+) to (\d+)/, i)
+        [a, b, c] |> Enum.map(fn i -> Integer.parse(i) |> elem(0) end)
+      end
+
+    {map, inst}
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -4,19 +4,27 @@ defmodule AdventOfCode.Day05Test do
   import AdventOfCode.Day05
 
   @input """
+      [D]
+  [N] [C]
+  [Z] [M] [P]
+   1   2   3
+
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
   """
 
-  @tag :skip
   test "part1" do
     result = part1(@input)
 
-    assert result
+    assert result == "CMZ"
   end
 
   @tag :skip
   test "part2" do
     result = part2(@input)
 
-    assert result
+    assert result == "MCD"
   end
 end

70cc5c91d34304e15f38faa45cfac34bb5495633
Julian Köpke <julian.koepke@gmail.com>
2022-12-09 09:02:35 +0100

    day 09

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,49 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    for m <- parse(args), reduce: {MapSet.new(), {0, 0}, {0, 0}} do
+      {pos, head, tail} ->
+        nhead = move(head, m)
+        ntail = follow(nhead, tail)
+        {MapSet.put(pos, ntail), nhead, ntail}
+    end
+    |> elem(0)
+    |> MapSet.size()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    for m <- parse(args), reduce: {MapSet.new(), {0, 0}, List.duplicate({0, 0}, 9)} do
+      {pos, head, tails} ->
+        nhead = move(head, m)
+
+        {ntails, last} =
+          Enum.map_reduce(tails, nhead, fn t, h ->
+            f = follow(h, t)
+            {f, f}
+          end)
+
+        {MapSet.put(pos, last), nhead, ntails}
+    end
+    |> elem(0)
+    |> MapSet.size()
+  end
+
+  defp move({x, y}, "R"), do: {x + 1, y}
+  defp move({x, y}, "U"), do: {x, y - 1}
+  defp move({x, y}, "L"), do: {x - 1, y}
+  defp move({x, y}, "D"), do: {x, y + 1}
+
+  defp follow({hx, hy}, {tx, ty}) when abs(hx - tx) <= 1 and abs(hy - ty) <= 1, do: {tx, ty}
+  defp follow({hx, hy}, {tx, ty}), do: {tx + sign(hx, tx), ty + sign(hy, ty)}
+
+  defp sign(a, b) when a > b, do: 1
+  defp sign(a, b) when a < b, do: -1
+  defp sign(_, _), do: 0
+
+  defp parse(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn s -> String.split(s, " ") end)
+    |> Enum.flat_map(fn [d, c] -> List.duplicate(d, Integer.parse(c) |> elem(0)) end)
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,19 +3,35 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  R 4
+  U 4
+  L 3
+  D 1
+  R 4
+  D 1
+  L 5
+  R 2
+  """
 
-    assert result
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 13
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    assert part2(@input) == 1
 
-    assert result
+    assert part2("""
+           R 5
+           U 8
+           L 8
+           D 3
+           R 17
+           D 10
+           L 25
+           U 20
+           """) == 36
   end
 end

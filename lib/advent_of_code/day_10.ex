759ae30b3af87846f7c9ed0740cea26a3cbc502c
Julian Köpke <julian.koepke@gmail.com>
2022-12-10 09:51:41 +0100

    day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  def part1(args) do
+    for f <- parse(args) |> Enum.flat_map(& &1), reduce: {1, 1, 0} do
+      {counter, register, acc} ->
+        acc =
+          if rem(counter, 40) == 20 do
+            acc + register * counter
+          else
+            acc
+          end
+
+        r = f.(register)
+        {counter + 1, r, acc}
+    end
+    |> elem(2)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    for f <- parse(args) |> Enum.flat_map(& &1), reduce: {0, 1, []} do
+      {counter, register, acc} ->
+        pos = rem(counter, 40)
+        acc = [(register >= pos - 1 && register <= pos + 1 && ?#) || ?. | acc]
+        r = f.(register)
+        {counter + 1, r, acc}
+    end
+    |> elem(2)
+    |> Enum.reverse()
+    |> Enum.chunk_every(40)
+    |> Enum.map(fn l -> to_string(l) <> "\n" end)
+    |> Enum.join()
+  end
+
+  defp parse(args) do
+    inst =
+      args
+      |> String.trim()
+      |> String.split("\n")
+
+    for i <- inst do
+      cond do
+        i == "noop" ->
+          [& &1]
+
+        ["addx", n] = String.split(i, " ") ->
+          {n, _} = Integer.parse(n)
+          [& &1, &(&1 + n)]
+      end
+    end
   end
 end
--- lib/mix/tasks/d10.p2.ex
+++ lib/mix/tasks/d10.p2.ex
@@ -12,6 +12,6 @@ defmodule Mix.Tasks.D10.P2 do
       else:
         input
         |> part2()
-        |> IO.inspect(label: "Part 2 Results")
+        |> IO.puts()
   end
 end

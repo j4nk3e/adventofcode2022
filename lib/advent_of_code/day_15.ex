dff0a0159b2dc75fa66bfe8c3a2a2095309cf422
Julian Köpke <julian.koepke@gmail.com>
2022-12-15 18:45:21 +0100

    day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,77 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  def part1(args, y \\ 2_000_000) do
+    grid = parse(args)
+
+    for {{sx, sy}, {bx, by}} <- grid do
+      d = abs(sx - bx) + abs(sy - by)
+      q = d - abs(y - sy)
+
+      if q < 0 do
+        nil
+      else
+        (sx - q)..(sx + q - 1)
+      end
+    end
+    |> Enum.filter(& &1)
+    |> Enum.flat_map(& &1)
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
-  def part2(_args) do
+  def remove([], _), do: []
+
+  def remove([{l, r} = h | tail], {a, b}) do
+    cond do
+      b < l -> [h | tail]
+      r < a -> [h | remove(tail, {a, b})]
+      a <= l && b < r -> [{b + 1, r} | tail]
+      a <= l && r <= b -> remove(tail, {a, b})
+      l < a && b < r -> [{l, a - 1}, {b + 1, r} | tail]
+      l < a && r <= b -> [{l, a - 1} | remove(tail, {a, b})]
+    end
+  end
+
+  defp hole(_, y, max) when y > max, do: []
+
+  defp hole(grid, y, max) do
+    ranges =
+      for {{sx, sy}, {bx, by}} <- grid, reduce: [{0, max}] do
+        ranges ->
+          d = abs(sx - bx) + abs(sy - by)
+          q = d - abs(y - sy)
+
+          if q < 0 do
+            ranges
+          else
+            remove(ranges, {sx - q, sx + q})
+          end
+      end
+
+    if Enum.empty?(ranges) do
+      hole(grid, y + 1, max)
+    else
+      {ranges |> hd |> elem(0), y}
+    end
+  end
+
+  def part2(args, max \\ 4_000_000) do
+    grid = parse(args)
+
+    {x, y} = hole(grid, 0, max)
+    y + 4_000_000 * x
+  end
+
+  defp parse(args) do
+    for l <- args |> String.trim() |> String.split("\n") do
+      [sx, sy, bx, by] =
+        Regex.run(
+          ~r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
+          l,
+          capture: :all_but_first
+        )
+        |> Enum.map(fn i -> Integer.parse(i) |> elem(0) end)
+
+      {{sx, sy}, {bx, by}}
+    end
   end
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -3,19 +3,46 @@ defmodule AdventOfCode.Day15Test do
 
   import AdventOfCode.Day15
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input "
+  Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+  Sensor at x=9, y=16: closest beacon is at x=10, y=16
+  Sensor at x=13, y=2: closest beacon is at x=15, y=3
+  Sensor at x=12, y=14: closest beacon is at x=10, y=16
+  Sensor at x=10, y=20: closest beacon is at x=10, y=16
+  Sensor at x=14, y=17: closest beacon is at x=10, y=16
+  Sensor at x=8, y=7: closest beacon is at x=2, y=10
+  Sensor at x=2, y=0: closest beacon is at x=2, y=10
+  Sensor at x=0, y=11: closest beacon is at x=2, y=10
+  Sensor at x=20, y=14: closest beacon is at x=25, y=17
+  Sensor at x=17, y=20: closest beacon is at x=21, y=22
+  Sensor at x=16, y=7: closest beacon is at x=15, y=3
+  Sensor at x=14, y=3: closest beacon is at x=15, y=3
+  Sensor at x=20, y=1: closest beacon is at x=15, y=3
+  "
 
-    assert result
+  test "part1" do
+    result = part1(@input, 10)
+
+    assert result == 26
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "remove" do
+    assert remove([{1, 9}, {99, 100}], {1, 9}) == [{99, 100}]
+    assert remove([{1, 9}, {99, 100}], {0, 10}) == [{99, 100}]
+    assert remove([{0, 9}, {99, 100}], {2, 9}) == [{0, 1}, {99, 100}]
+    assert remove([{0, 9}, {99, 100}], {8, 9}) == [{0, 7}, {99, 100}]
+    assert remove([{0, 9}, {99, 100}], {2, 8}) == [{0, 1}, {9, 9}, {99, 100}]
+    assert remove([{0, 9}, {99, 100}], {9, 10}) == [{0, 8}, {99, 100}]
+    assert remove([{0, 9}, {99, 100}], {0, 0}) == [{1, 9}, {99, 100}]
 
-    assert result
+    assert remove([{1, 9}], {1, 1}) == [{2, 9}]
+    assert remove([{1, 9}], {2, 2}) == [{1, 1}, {3, 9}]
+    assert remove([{1, 9}], {9, 9}) == [{1, 8}]
+  end
+
+  test "part2" do
+    result = part2(@input, 20)
+
+    assert result == 56_000_011
   end
 end
